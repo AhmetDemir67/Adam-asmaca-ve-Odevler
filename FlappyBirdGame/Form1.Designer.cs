44920b153a6376244ae7b76e9b67594a5139e27b
AhmetDemir67 <147312042+AhmetDemir67@users.noreply.github.com>
2024-10-30 07:20:17 +0300

    Add files via upload

---
+++ FlappyBirdGame/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace FlappyBirdOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ScoreText = new System.Windows.Forms.Label();
+            this.BoruUst3 = new System.Windows.Forms.PictureBox();
+            this.BoruAlt3 = new System.Windows.Forms.PictureBox();
+            this.BoruAlt2 = new System.Windows.Forms.PictureBox();
+            this.BoruUst2 = new System.Windows.Forms.PictureBox();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.Zemin = new System.Windows.Forms.PictureBox();
+            this.BoruAlt = new System.Windows.Forms.PictureBox();
+            this.BoruUst = new System.Windows.Forms.PictureBox();
+            this.BoruUst5 = new System.Windows.Forms.PictureBox();
+            this.BoruUst4 = new System.Windows.Forms.PictureBox();
+            this.BoruAlt5 = new System.Windows.Forms.PictureBox();
+            this.BoruAlt4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zemin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // ScoreText
+            // 
+            this.ScoreText.AutoSize = true;
+            this.ScoreText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ScoreText.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ScoreText.Location = new System.Drawing.Point(24, 553);
+            this.ScoreText.Name = "ScoreText";
+            this.ScoreText.Size = new System.Drawing.Size(36, 38);
+            this.ScoreText.TabIndex = 9;
+            this.ScoreText.Text = "0";
+            // 
+            // BoruUst3
+            // 
+            this.BoruUst3.Image = global::FlappyBirdGame.Properties.Resources.pipedown;
+            this.BoruUst3.Location = new System.Drawing.Point(879, -3);
+            this.BoruUst3.Name = "BoruUst3";
+            this.BoruUst3.Size = new System.Drawing.Size(100, 211);
+            this.BoruUst3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruUst3.TabIndex = 8;
+            this.BoruUst3.TabStop = false;
+            // 
+            // BoruAlt3
+            // 
+            this.BoruAlt3.Image = global::FlappyBirdGame.Properties.Resources.pipe;
+            this.BoruAlt3.Location = new System.Drawing.Point(879, 349);
+            this.BoruAlt3.Name = "BoruAlt3";
+            this.BoruAlt3.Size = new System.Drawing.Size(100, 161);
+            this.BoruAlt3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruAlt3.TabIndex = 7;
+            this.BoruAlt3.TabStop = false;
+            // 
+            // BoruAlt2
+            // 
+            this.BoruAlt2.Image = global::FlappyBirdGame.Properties.Resources.pipe;
+            this.BoruAlt2.Location = new System.Drawing.Point(666, 415);
+            this.BoruAlt2.Name = "BoruAlt2";
+            this.BoruAlt2.Size = new System.Drawing.Size(100, 95);
+            this.BoruAlt2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruAlt2.TabIndex = 6;
+            this.BoruAlt2.TabStop = false;
+//            this.BoruAlt2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // BoruUst2
+            // 
+            this.BoruUst2.Image = global::FlappyBirdGame.Properties.Resources.pipedown;
+            this.BoruUst2.Location = new System.Drawing.Point(666, -3);
+            this.BoruUst2.Name = "BoruUst2";
+            this.BoruUst2.Size = new System.Drawing.Size(100, 229);
+            this.BoruUst2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruUst2.TabIndex = 5;
+            this.BoruUst2.TabStop = false;
+//            this.BoruUst2.Click += new System.EventHandler(this.BoruUst2_Click);
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = global::FlappyBirdGame.Properties.Resources.bird;
+            this.flappyBird.Location = new System.Drawing.Point(116, 200);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(61, 58);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flappyBird.TabIndex = 1;
+            this.flappyBird.TabStop = false;
+            // 
+            // Zemin
+            // 
+            this.Zemin.Image = global::FlappyBirdGame.Properties.Resources.ground;
+            this.Zemin.Location = new System.Drawing.Point(1, 507);
+            this.Zemin.Name = "Zemin";
+            this.Zemin.Size = new System.Drawing.Size(1459, 102);
+            this.Zemin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Zemin.TabIndex = 3;
+            this.Zemin.TabStop = false;
+            // 
+            // BoruAlt
+            // 
+            this.BoruAlt.Image = global::FlappyBirdGame.Properties.Resources.pipe;
+            this.BoruAlt.Location = new System.Drawing.Point(446, 349);
+            this.BoruAlt.Name = "BoruAlt";
+            this.BoruAlt.Size = new System.Drawing.Size(100, 161);
+            this.BoruAlt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruAlt.TabIndex = 2;
+            this.BoruAlt.TabStop = false;
+            // 
+            // BoruUst
+            // 
+            this.BoruUst.Image = global::FlappyBirdGame.Properties.Resources.pipedown;
+            this.BoruUst.Location = new System.Drawing.Point(446, -3);
+            this.BoruUst.Name = "BoruUst";
+            this.BoruUst.Size = new System.Drawing.Size(100, 129);
+            this.BoruUst.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruUst.TabIndex = 0;
+            this.BoruUst.TabStop = false;
+            // 
+            // BoruUst5
+            // 
+            this.BoruUst5.Image = global::FlappyBirdGame.Properties.Resources.pipedown;
+            this.BoruUst5.Location = new System.Drawing.Point(1310, -3);
+            this.BoruUst5.Name = "BoruUst5";
+            this.BoruUst5.Size = new System.Drawing.Size(100, 196);
+            this.BoruUst5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruUst5.TabIndex = 10;
+            this.BoruUst5.TabStop = false;
+            // 
+            // BoruUst4
+            // 
+            this.BoruUst4.Image = global::FlappyBirdGame.Properties.Resources.pipedown;
+            this.BoruUst4.Location = new System.Drawing.Point(1115, -3);
+            this.BoruUst4.Name = "BoruUst4";
+            this.BoruUst4.Size = new System.Drawing.Size(100, 283);
+            this.BoruUst4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruUst4.TabIndex = 11;
+            this.BoruUst4.TabStop = false;
+            // 
+            // BoruAlt5
+            // 
+            this.BoruAlt5.Image = global::FlappyBirdGame.Properties.Resources.pipe;
+            this.BoruAlt5.Location = new System.Drawing.Point(1310, 366);
+            this.BoruAlt5.Name = "BoruAlt5";
+            this.BoruAlt5.Size = new System.Drawing.Size(100, 144);
+            this.BoruAlt5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruAlt5.TabIndex = 12;
+            this.BoruAlt5.TabStop = false;
+            // 
+            // BoruAlt4
+            // 
+            this.BoruAlt4.Image = global::FlappyBirdGame.Properties.Resources.pipe;
+            this.BoruAlt4.Location = new System.Drawing.Point(1115, 452);
+            this.BoruAlt4.Name = "BoruAlt4";
+            this.BoruAlt4.Size = new System.Drawing.Size(100, 58);
+            this.BoruAlt4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BoruAlt4.TabIndex = 13;
+            this.BoruAlt4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1458, 611);
+            this.Controls.Add(this.BoruAlt4);
+            this.Controls.Add(this.BoruAlt5);
+            this.Controls.Add(this.BoruUst4);
+            this.Controls.Add(this.BoruUst5);
+            this.Controls.Add(this.ScoreText);
+            this.Controls.Add(this.BoruUst3);
+            this.Controls.Add(this.BoruAlt3);
+            this.Controls.Add(this.BoruAlt2);
+            this.Controls.Add(this.BoruUst2);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.Zemin);
+            this.Controls.Add(this.BoruAlt);
+            this.Controls.Add(this.BoruUst);
+            this.Name = "Form1";
+            this.Text = "   ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Zemin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruUst4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BoruAlt4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BoruUst;
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox BoruAlt;
+        private System.Windows.Forms.PictureBox Zemin;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox BoruUst2;
+        private System.Windows.Forms.PictureBox BoruAlt2;
+        private System.Windows.Forms.PictureBox BoruAlt3;
+        private System.Windows.Forms.PictureBox BoruUst3;
+        private System.Windows.Forms.Label ScoreText;
+        private System.Windows.Forms.PictureBox BoruUst5;
+        private System.Windows.Forms.PictureBox BoruUst4;
+        private System.Windows.Forms.PictureBox BoruAlt5;
+        private System.Windows.Forms.PictureBox BoruAlt4;
+    }
+}
+
---
+++ FlappyBirdGame/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBirdOyunu
+{
+    public partial class Form1 : Form
+    {
+        // random: Rastgele sayı üretmek için kullanılır. Boruların konumları rastgele belirlenirken bu nesne kullanılır.
+        Random random = new Random();
+
+
+        // boruHızı: Boruların ekranda sola doğru hareket etme hızını belirler.
+        // gravity: Kuşun yerçekimi etkisi altında aşağı doğru hareket etme hızını belirler.
+        // score: Oyuncunun puanını tutar.
+        int boruHızı = 6;
+        int gravity = 3;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        // Oyun döngüsü. Belirli aralıklarla tetiklenir ve oyunun her karesi için yapılması gereken işlemleri içerir.
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            // Kuşu yerçekimi etkisi altında aşağı doğru hareket ettirir.
+            flappyBird.Top += gravity;
+
+            // Tüm boruları sola doğru hareket ettirir.
+            BoruAlt.Left -= boruHızı;
+            BoruUst.Left -= boruHızı;
+            BoruAlt2.Left -= boruHızı;
+            BoruUst2.Left -= boruHızı;
+            BoruAlt3.Left -= boruHızı;
+            BoruUst3.Left -= boruHızı;
+            BoruAlt4.Left -= boruHızı;
+            BoruUst4.Left -= boruHızı;
+            BoruAlt5.Left -= boruHızı;
+            BoruUst5.Left -= boruHızı;
+
+            ScoreText.Text = Convert.ToString(score);
+
+            // Borular ekranın solundan çıkarsa tekrar sağa taşır ve skoru artırır.
+            if (BoruUst.Left < -50)
+            {
+                BoruUst.Left = 800;
+
+            }
+
+            if (BoruAlt.Left < -50)
+            {
+                BoruAlt.Left = 800;
+                score++;
+            }
+
+            if (BoruUst2.Left < -50)
+            {
+                BoruUst2.Left = 800;
+
+            }
+
+            if (BoruAlt2.Left < -50)
+            {
+                BoruAlt2.Left = 800;
+                score++;
+
+            }
+
+            if (BoruUst3.Left < -50)
+            {
+                BoruUst3.Left = 800;
+
+            }
+
+            if (BoruAlt3.Left < -50)
+            {
+                BoruAlt3.Left = 800;
+                score++;
+            }
+
+            if (BoruUst4.Left < -50)
+            {
+                BoruUst4.Left = 800;
+
+            }
+
+            if (BoruAlt4.Left < -50)
+            {
+                BoruAlt4.Left = 800;
+                score++;
+            }
+
+            if (BoruUst5.Left < -50)
+            {
+                BoruUst5.Left = 800;
+
+            }
+
+            if (BoruAlt5.Left < -50)
+            {
+                BoruAlt5.Left = 800;
+                score++;
+            }
+
+
+
+            // Kuşun borulara veya zemine çarpıp çarpmadığını kontrol eder.
+
+            if (flappyBird.Bounds.IntersectsWith(BoruAlt.Bounds) || flappyBird.Bounds.IntersectsWith(BoruUst.Bounds) || flappyBird.Bounds.IntersectsWith(Zemin.Bounds)
+                || flappyBird.Bounds.IntersectsWith(BoruAlt2.Bounds) || flappyBird.Bounds.IntersectsWith(BoruUst2.Bounds) || flappyBird.Bounds.IntersectsWith(BoruAlt3.Bounds) || flappyBird.Bounds.IntersectsWith(BoruUst3.Bounds)
+                || flappyBird.Bounds.IntersectsWith(BoruAlt4.Bounds) || flappyBird.Bounds.IntersectsWith(BoruUst4.Bounds) || flappyBird.Bounds.IntersectsWith(BoruUst5.Bounds) || flappyBird.Bounds.IntersectsWith(BoruAlt.Bounds))
+            {
+                // Oyunu bitirir.
+                endGame();
+            }
+            /* if (score > 5)
+                 boruHızı = 10;
+             if (score > 12)
+                 boruHızı = 15;
+             if (score > 25)
+                 boruHızı = 25;
+             if (score > 100)
+                 boruHızı = 50;*/
+
+
+
+        }
+
+
+        // Boşluk tuşuna basıldığında tetiklenir. Kuşu yukarı doğru sıçratır.
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                gravity = -8;     // Yerçekimi etkisi geçici olarak azaltılır.
+        }
+
+        // Boşluk tuşu bırakıldığında tetiklenir. Kuşun normal hareketine devam etmesini sağlar.
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                gravity = 3;    // Yerçekimi etkisi eski haline getirilir.
+        }
+
+        // Oyun bittiğinde çağrılır. Oyunu durdurur ve sonucu gösterir.
+        private void endGame()
+        {
+            gameTimer.Stop();
+            ScoreText.Text = "SKORUNUZ:" + score;
+            //MessageBox.Show("SKORUNUZ:" + score);
+        }
+
+
+
+
+
+
+    }
+}
